9dd4ddd18da0010d42f9762c613d08292f951a3d
Max Jarvis <mwj22@miami.edu>
2018-10-10 10:49:26 -0400

    week 1

---
+++ sketch.js
@@ -0,0 +1,14 @@
+function setup() {
+  // put setup code here
+createCanvas(800, 400);
+background(220);
+
+
+}
+
+function draw() {
+ellipse(200, 200, 200, 100);
+ellipse(200, 210, 120, 30);
+ellipse(150, 150, 30, 30);
+ellipse(250, 150, 30, 30);
+}
